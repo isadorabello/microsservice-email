28073858bef7c228003dcafb919fc28a87b4a106
isadorabello <isadora.bello@gec.inatel.br>
2025-08-12 08:54:39 -0300

    first commit

---
+++ src/main/java/io/github/isadorabello/microsservice_email/MicrosserviceEmailApplication.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.microsservice_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/isadorabello/microsservice_email/MicrosserviceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.microsservice_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
